54aae2c9ddda84ba6ade78660d6c5d886e920cb9
Syed Hassaan Saleem <saleemhassaan94@gmail.com>
2023-03-12 21:02:56 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Hello GeeksForGeeks !!!")
+st.title("Pakistani News...")
+
+
+query = st.text_input("Enter Your Query", "Type Here ...")
+ 
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    result = name.title()
+    st.success(result)
